e122957fedd2cdd94f169364f5a804f7c4de7be3
Yan Li <me+git@yanli.dev>
2022-08-03 22:20:18 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+	<div class="weather-widget">
+		<RouterView />
+	</div>
+</template>
+
+<style scoped lang="scss">
+.weather-widget {
+	background-color: red;
+	height: 100px;
+	width: 80px;
+}
+</style>
